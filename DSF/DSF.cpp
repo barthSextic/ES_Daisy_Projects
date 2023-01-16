4f19b5a4f32fb09d667f3549c35bd3a170551abf
Rick Howell <88335430+barthSextic@users.noreply.github.com>
2023-01-16 14:07:34 -0500

    Working with LED

--- DSF/DSF.cpp
+++ DSF/DSF.cpp
@@ -18,16 +18,20 @@ using namespace daisysp;
     CV_8 < Spread
 */
 
-const float CUTOFF_MIN       = 20.0;
-const float CUTOFF_MAX       = 20000.0;
-const float RESONANCE_MAX    = 1.0;
+const float CUTOFF_MIN       = 20.0f;
+const float CUTOFF_MAX       = 20000.0f;
+const float CUTOFF_RANGE     = 20000.0f;
+const float RESONANCE_MAX    = 1.0f;
+const float BASE_1           = 4.0f;
+const float BASE_2           = 1.7f;
 
 DaisyPatchSM patch;
 Svf svfL1, svfL2, svfR1, svfR2;
 
-float   cutoff, fmKnob, fmCV1, fmCV2, 
+float   cutoff, fmKnob, fmCV1, fmCV2, cutoffSquared, 
         resonance, rmKnob, rmCV, 
         spread, spreadKnob, spreadCV,
+        imageL1, imageL2, imageR1, imageR2, exponent, 
         samplerate;
 
 
@@ -62,33 +66,47 @@ void AudioCallback(AudioHandle::InputBuffer  in,
     spreadCV        = patch.GetAdcValue(CV_8);
 
     // COMBINE PARAMETERS
-    cutoff          = fmKnob + fmCV1 + fmCV2;
-    cutoff          = RestrictRange(cutoff, 0.0, 1.0);
-    cutoff          = cutoff * cutoff * CUTOFF_MAX;
-
-    resonance       = rmKnob + rmCV;
-    resonance       = RestrictRange(resonance, 0.0, 1.0);
-
-    spread          = RestrictRange(spreadKnob + spreadCV, 0.0, 1.0);
+    cutoff          = RestrictRange(fmKnob + fmCV1 + fmCV2, 0.0f, 1.0f);
+    resonance       = RestrictRange(rmKnob + rmCV, 0.0f, 1.0f);
+    spread          = RestrictRange(spreadKnob + spreadCV, 0.0f, 1.0f);
 
     // CALCULATE SPREAD MAP
+    // NORMALIZE SPREAD TO [-1, 1]
+    spread = (2.0f * spread) - 1.0f;
+    cutoffSquared = cutoff * cutoff;
+
+    // WRITE CUTOFF VALUE TO LED
+    patch.WriteCvOut(CV_OUT_2, cutoff * 3.5);
+    // COMPUTE IMAGES
+    imageL1 = RestrictRange(pow(BASE_1, -spread) * cutoffSquared, 0.0f, 1.0f) * CUTOFF_RANGE;
+    imageL2 = RestrictRange(pow(BASE_2, spread) * cutoffSquared, 0.0f, 1.0f) * CUTOFF_RANGE;
+    imageR1 = RestrictRange(pow(BASE_1, spread) * cutoffSquared, 0.0f, 1.0f) * CUTOFF_RANGE;
+    imageR2 = RestrictRange(pow(BASE_2, -spread) * cutoffSquared, 0.0f, 1.0f) * CUTOFF_RANGE;
 
     // SET FILTER VALUES
-    svfL1.SetFreq(cutoff);
-    svfR1.SetFreq(cutoff);
+    svfL1.SetFreq(imageL1);
+    svfL2.SetFreq(imageL2);
+    svfR1.SetFreq(imageR1);
+    svfR2.SetFreq(imageR2);
     
     svfL1.SetRes(resonance);
+    svfL2.SetRes(resonance);
     svfR1.SetRes(resonance);
+    svfR2.SetRes(resonance);
 
     // PROCESS SAMPLES
     for(size_t i = 0; i < size; i++)
     {
         
         svfL1.Process(in[0][i]);
+        svfL2.Process(in[0][i]);
         svfR1.Process(in[1][i]);
+        svfR2.Process(in[1][i]);
 
-        out[0][i] = svfL1.Low(); /**< Copy the left input to the left output */
-        out[1][i] = svfR1.Low(); /**< Copy the right input to the right output */
+        // COMBINE FILTERS
+
+        out[0][i] = (svfL1.Low() + svfL2.Low()) * 0.5f; 
+        out[1][i] = (svfR1.Low() + svfR2.Low()) * 0.5f; 
     }
 } // END AUDIO CALLBACK METHOD
 
@@ -101,7 +119,9 @@ int main(void)
     samplerate = patch.AudioSampleRate();
 
     svfL1.Init(samplerate);
+    svfL2.Init(samplerate);
     svfR1.Init(samplerate);
+    svfR2.Init(samplerate);
 
     /** Start Processing the audio */
     patch.StartAudio(AudioCallback);
